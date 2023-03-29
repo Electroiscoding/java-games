ee34acc4a462e91ee99186999959513ebfc229b6
Electroiscoding <103299713+Electroiscoding@users.noreply.github.com>
2023-03-29 20:56:33 +0530

    Create chatbot.java

---
+++ chatbot.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class chatbot
+{
+    public static void main(String args[])
+    {
+        String a,b,c,d,e,f;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("HELLO I AM JAVA-BOT, what's your name?");
+        a=sc.nextLine();
+        System.out.println("nice to know your name, but how are you?");
+        b=sc.nextLine();
+        System.out.println("what food do you like the most?");
+        c=sc.nextLine();
+        System.out.println("what's your favourite colour?");
+        d=sc.nextLine();
+        System.out.println("Do you know I am coded in JAVA");
+        e=sc.nextLine();
+        System.out.println("Nice to talk to you. Sadly I am coded till this much & thanks to talk with me");
+        f=sc.nextLine();
+        System.out.println("MADE BY SOHAM PAL/electroiscoding in github");
+    }
+}
